4534ad3ca8a76f8cace4eadee0feaec6280e3620
ngthecoder <150304266+ngthecoder@users.noreply.github.com>
2026-03-14 23:35:11 -0400

    feat: add movies domain with GET /movies and GET /movies/:id

--- main.go
+++ main.go
@@ -5,9 +5,21 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/ngthecoder/movie_ticketing/db"
+	"github.com/ngthecoder/movie_ticketing/movies"
 )
 
 func main() {
+	db, err := db.Connect()
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
+	defer db.Close()
+
+	movieRepository := movies.NewMovieRepository(db)
+	movieService := movies.NewMovieService(movieRepository)
+	movieHandler := movies.NewMovieHandler(movieService)
+
 	r := gin.Default()
 
 	r.GET("/ping", func(c *gin.Context) {
@@ -15,6 +27,10 @@ func main() {
 			"message": "pong",
 		})
 	})
+
+	r.GET("/movies", movieHandler.GetAllHandler)
+	r.GET("/movies/:id", movieHandler.GetByIdHandler)
+
 	if err := r.Run(":8080"); err != nil {
 		log.Fatalf("failed to run server: %v", err)
 	}
